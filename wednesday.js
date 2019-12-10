c5dcec0ffd2660afd0c0124e09950fed6e2564d4
Nicho Ferez <Nickferez@yahoo.com>
2019-12-10 11:36:38 -0500

    jnklbjkbj,
    nbkb,jbjn,nb

---
+++ wednesday.js
@@ -0,0 +1,9 @@
+$("#lightoff").click(function(){
+  $("#lightoff").toggleClass("on");
+  $(".room1").toggleClass("show");
+});
+
+$("#lightoff-2").click(function(){
+  $("#lightoff-2").toggleClass("on");
+  $(".room2").toggleClass("show");
+});
