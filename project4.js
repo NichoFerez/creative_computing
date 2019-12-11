dc83ce9980728c54a8140d0b428d5dbaab3ae55e
Nicho Ferez <Nickferez@yahoo.com>
2019-12-10 20:58:32 -0500

    mklnk
     bjkl,

---
+++ project4.js
@@ -0,0 +1,19 @@
+$(".grid_child1").click(function(){
+  $("#grid_child1").toggleClass("open");
+});
+
+$(".grid_child2").click(function(){
+  $(".grid_child2").toggleClass("open");
+});
+
+$(".grid_child3").click(function(){
+  $(".grid_child3").toggleClass("open");
+});
+
+$(".grid_child4").click(function(){
+  $("grid_child4").toggleClass("open");
+});
+
+$(".grid_child5").click(function(){
+  $(".grid_child5").toggleClass("open");
+});
